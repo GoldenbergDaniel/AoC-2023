caa2f23bf71e571a80ffbec16bcdb25d9ac38efe
DanielGoldenberg <danielgoldenberg0@outlook.com>
2023-12-02 02:12:36 -0500

    day 1 final cleanup

--- day1/main.c
+++ day1/main.c
@@ -12,14 +12,13 @@ i32 main(void)
     str("six"), str("seven"), str("eight"), str("nine"), str("ten")
   };
 
-  String res_str = str_alloc(16);
-  i32 res_num = 0;
-  u8 res_idx = 0;
   i32 sum = 0;
+  String nums_str = str_alloc(16);
 
   String line = str_alloc(1024);
   while (fgets(line.str, 1024, stdin) != NULL)
   {
+    u8 nums_idx = 0;
     line.len = cstr_len(line.str)-1;
 
     for (u32 i = 0; i < line.len; i++)
@@ -27,39 +26,37 @@ i32 main(void)
       i8 c = line.str[i];
       if (c >= 48 && c <= 57)
       {
-        res_str.str[res_idx] = c;
-        res_idx++;
+        nums_str.str[nums_idx] = c;
+        nums_idx++;
       }
 
       for (u8 j = 0; j < 10; j++)
       {
-        if (line.len < table[j].len+i) continue;
+        if (line.len < table[j].len + i) continue;
 
-        String substr = str_substr(line, i, i + table[j].len );
+        String substr = str_substr(line, i, i + table[j].len);
         if (str_contains(substr, table[j]))
         {
-          res_str.str[res_idx] = j + 49;
-          i += table[j].len - 2;
-          res_idx++;
+          nums_str.str[nums_idx] = j+49;
+          i += table[j].len-2;
+          nums_idx++;
         }
       }
     }
 
-    res_num = res_num * 10 + (res_str.str[0] - 48);
+    i32 res_num = 0;
+    res_num = res_num * 10 + (nums_str.str[0] - 48);
 
-    if (res_idx == 1)
+    if (nums_idx == 1)
     {
-      res_num = res_num * 10 + (res_str.str[0] - 48);
+      res_num = res_num * 10 + (nums_str.str[0] - 48);
     }
     else
     {
-      res_num = res_num * 10 + (res_str.str[res_idx-1] - 48);
+      res_num = res_num * 10 + (nums_str.str[nums_idx-1] - 48);
     }
 
     sum += res_num;
-
-    res_num = 0;
-    res_idx = 0;
   }
 
   printf("%i\n", sum);
