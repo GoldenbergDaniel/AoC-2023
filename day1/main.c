806676c7bf912bcaa6ae454d7f5c95cd11ee1b01
DanielGoldenberg <danielgoldenberg0@outlook.com>
2023-12-02 01:04:09 -0500

    day 1 solved

--- day1/main.c
+++ day1/main.c
@@ -1,63 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 #include "../common.h"
-
-u64 get_stream_len(FILE *stream);
+#include "../string.h"
 
 i32 main(void)
 {
-  u64 in_len = get_stream_len(stdin);
-  
-  i8 *in = malloc(in_len);
-  fread(in, 1, in_len, stdin);
-  in = strncat(in, "\n", in_len);
-  in_len++;
+  String table[11] = {{0}, str("one"), str("two"), str("three"), str("four"), str("five"),
+                           str("six"), str("seven"), str("eight"), str("nine"), str("ten")};
 
-  i8 *res_str = malloc(16);
+  FILE *in = stdin;
+
+  String res_str = str_alloc(16);
   i32 res_num = 0;
   u8 res_idx = 0;
   i32 sum = 0;
 
-  for (u32 i = 0; i < in_len; i++)
+  String line = str_alloc(1024);
+  while (fgets(line.str, 1024, in) != NULL)
   {
-    i8 c = in[i];
+    line.len = cstr_len(line.str)-1;
 
-    if (c >= 48 && c <= 57)
+    for (u32 i = 0; i < line.len; i++)
     {
-      res_str[res_idx] = c;
-      res_idx++;
-    }
-
-    if (in[i] == '\n')
-    {
-      res_num = res_num * 10 + (res_str[0] - 48);
-
-      if (res_idx == 1)
+      i8 c = line.str[i];
+      if (c >= 48 && c <= 57)
       {
-        res_num = res_num * 10 + (res_str[0] - 48);
-      }
-      else
-      {
-        res_num = res_num * 10 + (res_str[res_idx-1] - 48);
+        res_str.str[res_idx] = c;
+        res_idx++;
       }
 
-      sum += res_num;
-
-      res_num = 0;
-      res_idx = 0;
+      for (u8 j = 1; j < 11; j++)
+      {
+        u64 end = line.len < table[j].len+i ? line.len : table[j].len+i;
+        String substr = str_substr(line, i, end);
+        if (str_contains(substr, table[j]))
+        {
+          res_str.str[res_idx] = j + 48;
+          i += table[j].len - 2;
+          res_idx++;
+        }
+      }
     }
+
+    res_num = res_num * 10 + (res_str.str[0] - 48);
+
+    if (res_idx == 1)
+    {
+      res_num = res_num * 10 + (res_str.str[0] - 48);
+    }
+    else
+    {
+      res_num = res_num * 10 + (res_str.str[res_idx-1] - 48);
+    }
+
+    sum += res_num;
+
+    res_num = 0;
+    res_idx = 0;
   }
 
   printf("%i\n", sum);
-}
-
-u64 get_stream_len(FILE *stream)
-{
-  fseek(stdin, 0, SEEK_END);
-  u64 len = ftell(stdin);
-  fseek(stdin, 0, SEEK_SET);
-
-  return len;
 }
